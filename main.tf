da1cef2627fbef336eb0daea0d78ea91812d87b8
Nico Vibert <nvibert@vmware.com>
2021-02-22 21:07:45 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "vsphere" {
+user                 = "cloudadmin@vmc.local"
+password             = ""
+vsphere_server       = ""
+allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+name = "SDDC-Datacenter"
+}
+
+
+resource "vsphere_folder" "folder" {
+path          = "your_user_name_terraform_folder"
+type          = "vm"
+datacenter_id = data.vsphere_datacenter.dc.id
+}
