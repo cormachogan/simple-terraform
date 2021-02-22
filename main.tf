59781b1c17a4a864ca95ae2813176fa29c9fb280
Nico Vibert <nvibert@vmware.com>
2021-02-22 21:10:24 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 provider "vsphere" {
-user                 = "cloudadmin@vmc.local"
-password             = ""
-vsphere_server       = ""
-allow_unverified_ssl = true
+  user                 = "cloudadmin@vmc.local"
+  password             = ""
+  vsphere_server       = ""
+  allow_unverified_ssl = true
 }
 
 data "vsphere_datacenter" "dc" {
-name = "SDDC-Datacenter"
+  name = "SDDC-Datacenter"
 }
 
 
 resource "vsphere_folder" "folder" {
-path          = "your_user_name_terraform_folder"
-type          = "vm"
-datacenter_id = data.vsphere_datacenter.dc.id
+  path          = "your_user_name_terraform_folder"
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
